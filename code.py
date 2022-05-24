052922c43020d0f2df05187ef2d234318d4be870
EslamAbolfadl <73839168+Eslam-AbolFadl@users.noreply.github.com>
2022-05-24 00:01:30 +0000

    Update code.py

--- code.py
+++ code.py
@@ -13,8 +13,8 @@ def pipeline (image):
     image=cv2.resize(image,(416,416))
  
  
-    weightspath=os.path.join("/home/saf/yolo-det/projenv/bin","yolov3.weights")
-    configpath=os.path.join("/home/saf/yolo-det/projenv/bin","yolov3.cfg")
+    weightspath=os.path.join("/home/saf/yolo-det/projenv/bin","yolov3-tiny.weights")
+    configpath=os.path.join("/home/saf/yolo-det/projenv/bin","yolov3-tiny.cfg")
     net= cv2.dnn.readNetFromDarknet(configpath,weightspath)
 
 
@@ -22,15 +22,14 @@ def pipeline (image):
 
     layers=net.getLayerNames()
     outlayers= [layers[i - 1] for i in net.getUnconnectedOutLayers()]
+   
     # Load names of classes 
 
     classes= open('/home/saf/yolo-det/projenv/bin/coco_classes').read().strip().split('\n')
-#     np.random.seed(25)
-#     global colors 
-#     colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
 
 
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    
     # construct a blob from the image
     blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
     r = blob[0, 0, :, :]
@@ -53,7 +52,7 @@ def pipeline (image):
             scores = detection[5:]
             classID = np.argmax(scores)
             confidence = scores[classID]
-            if confidence > 0.7:
+            if confidence > 0.7:    ##only high score boxes will be kept, others will be removed
                 box = detection[:4] * np.array([w, h, w, h])
                 (centerX, centerY, width, height) = box.astype("int")
                 x = int(centerX - (width / 2))
@@ -64,16 +63,16 @@ def pipeline (image):
                 classIDs.append(classID)
 
     indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    l=0
-    if len(indices) > 0:
+    l=0     #to insert objects on top of the image
+    if len(indices) > 0:  # if NMSBoxes has output ,then  insert boxes, classes, and scores of the detected objects
         for i in indices.flatten():
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
         
             #color = [int(c) for c in colors[classIDs[i]]]
-            cv2.rectangle(image, (x, y), (x + w, y + h), (10,10,255), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255,10,10), 2)
             text = "{}: {:.4f}".format(classes[classIDs[i]], confidences[i])
-            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (10,10,255), 1)
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,10,10), 1)
             
             
             # extract detected vehicles and instert them on the top of the image
@@ -82,6 +81,7 @@ def pipeline (image):
             image[0:rows, cols*(2*l):cols*(2*l+1) ] = cropim
             l=l+1
     return image           
+
 
 
 
